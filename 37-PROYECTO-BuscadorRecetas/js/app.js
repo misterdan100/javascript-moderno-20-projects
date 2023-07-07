98685404770cb152332f9530855a72e7b6c974ae
misterdan100 <danielmca60@gmail.com>
2023-07-06 22:44:51 -0500

    Working in Buscador de Recetas Project on JS

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -4,6 +4,7 @@ function iniciarApp() {
     selectCategorias.addEventListener('change', seleccionarCategoria)
 
     const resultado = document.querySelector('#resultado');
+    const modal = new bootstrap.Modal('#modal', {})
 
     obtenerCategorias();
 
@@ -95,7 +96,93 @@ function iniciarApp() {
 
     function seleccionarReceta(id) {
         const url = `http://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-        console.log(url);
+        fetch(url)
+            .then( respuesta => respuesta.json())
+            .then( resultado => mostrarRecetaModal(resultado.meals[0]) )
+            .catch( console.log('error api lookup.php'))
+    }
+
+    function mostrarRecetaModal(receta) {
+
+        const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+
+        //* Aniadir contenido a modal
+        const modalTitle = document.querySelector('.modal .modal-title');
+        const modalBody = document.querySelector('.modal .modal-body');
+
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}"> 
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y Cantidades:</h3>
+
+        `;
+
+        const listGroup = document.createElement('UL');
+        listGroup.classList.add('list-group');
+        //* Mostrar cantidades e ingredientes
+        for(let i = 1; i <= 20; i++) {
+            if(receta[`strIngredient${i}`]) {
+                const ingrediente = receta[`strIngredient${i}`];
+                const cantidad = receta[`strMeasure${i}`];
+
+                const ingredienteLi = document.createElement('LI');
+                ingredienteLi.classList.add('list-group-item');
+                ingredienteLi.textContent = `${ingrediente} - ${cantidad}`; 
+
+                listGroup.appendChild(ingredienteLi);
+            }
+        }
+
+        modalBody.appendChild(listGroup)
+
+        const modalFooter = document.querySelector('.modal-footer');
+        limpiarHTML(modalFooter);
+
+        //* Botones de cerrar y favorito
+        const btnFavorito = document.createElement('BUTTON');
+        btnFavorito.classList.add('btn', 'btn-danger', 'col');
+        btnFavorito.textContent = 'Guardar Favorito';
+        
+        const btnCerrarModal = document.createElement('BUTTON');
+        btnCerrarModal.classList.add('btn', 'btn-secondary', 'col');
+        btnCerrarModal.textContent = 'Cerrar';
+        //btnCerrarModal.dataset.bsDismiss = "modal"; // atributo para cerrar
+        
+        
+        modalFooter.appendChild(btnFavorito);
+        modalFooter.appendChild(btnCerrarModal);
+        
+        //* 
+        btnCerrarModal.onclick = function() {
+            modal.hide();
+        }
+
+        //* Local Storage
+        btnFavorito.onclick = function() {
+            if(!existeStorage(idMeal)) {
+                agregarFavorito({
+                    id: idMeal,
+                    titulo: strMeal,
+                    img: strMealThumb
+                });
+            }
+
+        };
+
+        //* Muestra el modal
+        modal.show();
+    }
+
+    function agregarFavorito(receta) {
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        localStorage.setItem('favoritos', JSON.stringify([...favoritos, receta]));
+    }
+    
+    function existeStorage(id) {
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        return favoritos.some(favorito => favorito.id === id);
     }
 
     function limpiarHTML(selector) {
