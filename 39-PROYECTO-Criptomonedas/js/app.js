0aad9948ad738be1ee671345d1858681595ffe91
misterdan100 <danielmca60@gmail.com>
2023-07-08 16:55:36 -0500

    Creating simple cript exchange with API on JS

---
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -0,0 +1,162 @@
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedasSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: '',
+    monedaNombre: '',
+    criptoNombre: ''
+}
+
+//! Crear un promise
+const obtenerCriptomonedas = criptomonedas => new Promise((resolve) => {
+    resolve(criptomonedas);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+    monedasSelect.addEventListener('change', leerValor);
+    criptomonedasSelect.addEventListener('change', leerValor);
+})
+
+function consultarCriptomonedas() {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( resultado => obtenerCriptomonedas(resultado.Data))
+        .then( criptomonedas => selectCriptomonedas(criptomonedas) )
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach( cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('OPTION');
+        option.value = Name;
+        option.textContent = FullName;
+
+        criptomonedasSelect.appendChild(option)
+    })
+}
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+    console.log(e);
+    if(e.target.name === 'moneda') {
+        objBusqueda.monedaNombre = e.target.selectedOptions[0].text;
+    } else {
+        objBusqueda.criptoNombre = e.target.selectedOptions[0].text;
+    }
+    
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    //* Validar
+    const { moneda, criptomoneda } = objBusqueda;
+    if(moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+    //* Consultar la API con los resultados
+    consultarAPI();
+
+}
+
+function mostrarAlerta(mensaje) {
+    const existeError = document.querySelector('.error');
+    if(!existeError) {
+        const divMensaje = document.createElement('DIV');
+        divMensaje.classList.add('error');
+    
+        //* Mensaje de error
+        divMensaje.textContent = mensaje;
+        formulario.appendChild(divMensaje);
+    
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 2000);
+    }
+
+}
+
+function consultarAPI() {
+    const { moneda, criptomoneda } = objBusqueda;
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}
+    `;
+
+    mostrarSpinner();
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( cotizacion => {
+            mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+        })
+
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+    limpiarHTML();
+
+    const { PRICE, HIGHDAY, CHANGEPCT24HOUR, LOWDAY, LASTUPDATE } = cotizacion;
+
+    const precio = document.createElement('P');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+    const info = document.createElement('P');
+    info.classList.add('precio');
+    info.innerHTML = `De: <span>${objBusqueda.criptoNombre}</span> => <span>${objBusqueda.monedaNombre}</span>`;
+
+    const precioAlto = document.createElement('P');
+    precioAlto.innerHTML = `<p>Precio mas alto del dia: <span>${HIGHDAY}</span></p>`
+
+    const precioBajo = document.createElement('P');
+    precioBajo.innerHTML = `<p>Precio mas bajo del dia: <span>${LOWDAY}</span></p>`;
+
+    const ultimasHoras = document.createElement('P');
+    ultimasHoras.innerHTML = `<p>Variacion ultimas 24 horas: <span>${CHANGEPCT24HOUR}%</span></p>`;
+
+    const ultimasActualizacion = document.createElement('P');
+    ultimasActualizacion.innerHTML = `<p>Ultima actualizacion: <span>${LASTUPDATE}</span></p>`;
+
+    resultado.appendChild(info);
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimasActualizacion);
+
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarSpinner() {
+    limpiarHTML();
+
+    const spinner = document.createElement('DIV');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `;
+
+    resultado.appendChild(spinner);
+
+    setTimeout(() => {
+        spinner.remove();
+    }, 2000);
+}
