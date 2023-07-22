fddd88d7555ed3cb5c4770765438adbcfa3bd512
misterdan100 <danielmca60@gmail.com>
2023-07-22 08:31:14 -0500

    Starting React Introduction with project

---
+++ 54-Introducción-a-React-Creando-Un-Proyecto/cotizador-prestamos-react/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const a = 1;
+  const b = 2;
+  const resultado = a + b;
+
+  return (
+    <h1 className="text-6xl font-extrabold">Hello, world!</h1>
+  )
+}
+
+export default App
