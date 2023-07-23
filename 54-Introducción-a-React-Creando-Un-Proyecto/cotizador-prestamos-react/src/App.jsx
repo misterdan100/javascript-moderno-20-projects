0644dabe3d5857012f1f501ae2badbb44401a538
misterdan100 <danielmca60@gmail.com>
2023-07-22 19:27:14 -0500

    Learning about React, pops, functions...

--- 54-Introducción-a-React-Creando-Un-Proyecto/cotizador-prestamos-react/src/App.jsx
+++ 54-Introducción-a-React-Creando-Un-Proyecto/cotizador-prestamos-react/src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+import Header from "./components/Header";
+
 function App() {
-  const a = 1;
-  const b = 2;
-  const resultado = a + b;
+  const [ cantidad, setCantidad ] = useState(10000);
+
+  function handleCantidad(e) {
+    console.log(+e.target.value)
+  }
 
   return (
-    <h1 className="text-6xl font-extrabold">Hello, world!</h1>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10 rounded-xl">
+      <Header />
+
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleCantidad}
+      />
+      
+    </div>
   )
 }
 
