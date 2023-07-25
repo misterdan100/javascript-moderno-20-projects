b2accfb9cac9510edaeb26647ab55bb19d92246f
misterdan100 <danielmca60@gmail.com>
2023-07-24 22:44:32 -0500

    Starting Vue.js Introduction and simple project

---
+++ 55-Introduccion-Vue.js-Creando-un-Proyecto/cotizador-prestamos-vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Header from './components/Header.vue';
+
+  
+
+</script>
+
+<template>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10 rounded-3xl">
+    <Header />
+  </div>
+</template>
+
